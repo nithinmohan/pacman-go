6aa6d344d4d4bc75eb3891012b6c19ee65a02060
Nithin Mohan <nithinmohan94@gmail.com>
2019-03-21 21:11:48 +0530

    added animations accroding to direction

--- gogame.go
+++ gogame.go
@@ -4,12 +4,14 @@ import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	// "github.com/faiface/pixel/imdraw"
+	"fmt"
 	"image"
 	_ "image/png"
+	"math"
 	"os"
-	"fmt"
+	"time"
+
 	"golang.org/x/image/colornames"
-	// "math"
 )
 
 func getSheet(filePath string) (pixel.Picture, error) {
@@ -28,9 +30,9 @@ func getSheet(filePath string) (pixel.Picture, error) {
 	return sheet, nil
 }
 
-func getFrame(frameWidth float64, frameHeight float64, xGrid int, yGrid int) (pixel.Rect){
+func getFrame(frameWidth float64, frameHeight float64, xGrid int, yGrid int) pixel.Rect {
 	//https://github.com/faiface/pixel/wiki/Drawing-a-Sprite#picture
-	
+
 	return pixel.R(
 		float64(xGrid)*frameWidth,
 		float64(yGrid)*frameHeight,
@@ -40,38 +42,42 @@ func getFrame(frameWidth float64, frameHeight float64, xGrid int, yGrid int) (pi
 }
 
 type direction int
+
 const (
 	up direction = iota
 	down
 	left
 	right
 )
-type pacman struct{
+
+type pacman struct {
 	direction direction
-	anims map[direction][]pixel.Rect
-	rate float64
-	counter float64
-	frame pixel.Rect //stores current frame. updates in update function
-	sheet pixel.Picture //stores spritesheel in pixel picture format
-	pos pixel.Rect
+	anims     map[direction][]pixel.Rect
+	rate      float64
+	counter   float64
+	frame     pixel.Rect    //stores current frame. updates in update function
+	sheet     pixel.Picture //stores spritesheel in pixel picture format
+	pos       pixel.Rect
 }
-func (pm *pacman) load() error{
+
+func (pm *pacman) load() error {
 	var err error
-	pm.sheet, err=getSheet("spritemap-384.png")
-	if err!=nil{
+	pm.sheet, err = getSheet("spritemap-384.png")
+	if err != nil {
 		panic(err)
 	}
-	pm.pos=pixel.R(10,10,100,100)
+	pm.rate = 1/10.0
+	pm.pos = pixel.R(10, 10, 100, 100)
 	pm.anims = make(map[direction][]pixel.Rect)
-	pm.frame = getFrame(24,24,1,6)
-	pm.anims[up]=append(pm.anims[up],getFrame(24,24,1,6))
-	pm.anims[up]=append(pm.anims[up],getFrame(24,24,3,6))
-	pm.anims[down]=append(pm.anims[down],getFrame(24,24,5,6))
-	pm.anims[down]=append(pm.anims[down],getFrame(24,24,7,6))
-	pm.anims[left]=append(pm.anims[left],getFrame(24,24,0,6))
-	pm.anims[left]=append(pm.anims[left],getFrame(24,24,2,6))
-	pm.anims[right]=append(pm.anims[right],getFrame(24,24,4,6))
-	pm.anims[right]=append(pm.anims[right],getFrame(24,24,6,6))
+	pm.frame = getFrame(24, 24, 1, 6)
+	pm.anims[up] = append(pm.anims[up], getFrame(24, 24, 1, 6))
+	pm.anims[up] = append(pm.anims[up], getFrame(24, 24, 3, 6))
+	pm.anims[down] = append(pm.anims[down], getFrame(24, 24, 5, 6))
+	pm.anims[down] = append(pm.anims[down], getFrame(24, 24, 7, 6))
+	pm.anims[left] = append(pm.anims[left], getFrame(24, 24, 0, 6))
+	pm.anims[left] = append(pm.anims[left], getFrame(24, 24, 2, 6))
+	pm.anims[right] = append(pm.anims[right], getFrame(24, 24, 4, 6))
+	pm.anims[right] = append(pm.anims[right], getFrame(24, 24, 6, 6))
 	return nil
 
 }
@@ -89,6 +95,19 @@ func (pm *pacman) draw(t pixel.Target) {
 	// sprite := pixel.NewSprite(pm.sheet, pm.sheet.Bounds())
 	// sprite.Draw(t, pixel.IM)
 }
+func (pm *pacman) update(dt float64, directionValue direction) {
+	pm.counter = dt //why dt is based on time
+	pm.direction = directionValue
+	directionVecMap := make(map[direction]pixel.Vec)
+	directionVecMap[right] = pixel.V(1,0)
+	directionVecMap[left] = pixel.V(-1,0)
+	directionVecMap[up] = pixel.V(0,1)
+	directionVecMap[down] = pixel.V(0,-1) 
+	pm.pos = pm.pos.Moved(directionVecMap[directionValue])
+	i := int(math.Floor(pm.counter / pm.rate))
+	fmt.Println(i)
+	pm.frame = pm.anims[pm.direction][i%len(pm.anims[pm.direction])]
+}
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "Packman",
@@ -104,13 +123,16 @@ func run() {
 
 	pm := &pacman{}
 	err = pm.load()
-	if err!=nil{
+	if err != nil {
 		panic(err)
 	}
+	last := time.Now()
 	for !win.Closed() {
 		// pm.draw(win)
 		// imd.Draw(canvas)
+		dt := time.Since(last).Seconds()
 		win.Clear(colornames.Black)
+		pm.update(dt, right)
 		pm.draw(win)
 		// win.SetMatrix(pixel.IM.Scaled(pixel.ZV,
 		// 	math.Min(
